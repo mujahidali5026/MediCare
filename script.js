de959af6a8805ca182163dd6c326fc61f0bbcf6d
mujahidali5026 <mali7222741@gmail.com>
2023-08-05 10:17:24 -0700

    mediCare

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector("#menu-btn").onclick = () =>{
+    document.querySelector(".navbar").classList.toggle('active');
+    document.querySelector("#menu-btn").classList.toggle('#menu-btn');
+    document.querySelector("#menu-btn").classList.toggle('fa-times');
+  };
